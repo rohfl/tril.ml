938f83cd20c97be32327c2e62fc8bbe1300e46d0
rohfl <iamrohitjangid21@gmail.com>
2021-06-19 17:32:06 +0530

    Remove firebase config details.

---
+++ firebase/firebase.js
@@ -0,0 +1,11 @@
+var firebase = require("firebase/app");
+
+require("firebase/database");
+
+var firebaseConfig = {
+    
+};
+
+firebase.initializeApp(firebaseConfig);
+
+module.exports = firebase 
